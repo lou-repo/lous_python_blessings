dd8a88d1f3ced10faed845e813c6f175f90e6658
lou jr <louis@5servers.com>
2022-03-06 15:25:40 -0500

    fixed syntax errors

--- ssh_connect/ssh_connect.py
+++ ssh_connect/ssh_connect.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
+from tkinter import COMMAND
 import paramiko
 from paramiko import SSHClient
 import subprocess
 import time
 
-host = input("IP or FQDN: ")
-username = input("What is your username: " )
-port = input("SSH Port Number: ")
-command = "ls -l"
+HOST = input("IP or FQDN: ")
+USERNAME = input("What is your username: " )
+PORT = input("SSH Port Number: ")
+COMMAND = "ls -l"
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -17,10 +18,8 @@ stdin, stdout, stderr = ssh.exec_command(command)
 lines = stdout.readlines()
 
 def line_reader(lines):
-	for line in lines:
-		print(line.strip())
+    for line in lines:
+        print(line.strip())
 
 print(line_reader(lines))
 time.sleep(3)
-
-
