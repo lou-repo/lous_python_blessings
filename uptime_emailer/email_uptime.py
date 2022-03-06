1a89e054a35f305b672013f597de3da87d7000a3
lou jr <louis@5servers.com>
2022-03-06 17:23:19 -0500

    modified email uptime

--- uptime_emailer/email_uptime.py
+++ uptime_emailer/email_uptime.py
@@ -14,33 +14,33 @@ def uptime():
             return "Cannot open uptime file: /proc/uptime"
         total_seconds = float(contents[0])
     # Helper vars:
-MINUTE  = 60
-HOUR    = MINUTE * 60
-DAY     = HOUR * 24
+    MINUTE  = 60
+    HOUR    = MINUTE * 60
+    DAY     = HOUR * 24
     # Get the days, hours, etc:
-days    = int( total_seconds / DAY )
-hours   = int( ( total_seconds % DAY ) / HOUR )
-minutes = int( ( total_seconds % HOUR ) / MINUTE )
-seconds = int( total_seconds % MINUTE )
+    days    = int( total_seconds / DAY )
+    hours   = int( ( total_seconds % DAY ) / HOUR )
+    minutes = int( ( total_seconds % HOUR ) / MINUTE )
+    seconds = int( total_seconds % MINUTE )
 
 # Build up the pretty string (like this: "N days, N hours, N minutes, N seconds")
-string = ""
-    if days > 0:
-        string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
-    if len(string) > 0 or hours > 0:
-        string += str(hours) + " " + (hours == 1 and "hour" or "hours" ) + ", "
-    if len(string) > 0 or minutes > 0:
-        string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes" ) + ", "
-        string += str(seconds) + " " + (seconds == 1 and "second" or "seconds" )
-        return string;
+    string = ""
+        if days > 0:
+            string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
+        if len(string) > 0 or hours > 0:
+            string += str(hours) + " " + (hours == 1 and "hour" or "hours" ) + ", "
+        if len(string) > 0 or minutes > 0:
+            string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes" ) + ", "
+            string += str(seconds) + " " + (seconds == 1 and "second" or "seconds" )
+            return string;
 
-    print("The system uptime is: " + uptime())
+        print ("The system uptime is: " + uptime())
 
 #shows users logged in
 
 checkuser = getpass.getuser()
 
-print("The user currently logged in is: " + checkuser)
+print ("The user currently logged in is: " + checkuser)
 
 #This emails administrator with details above
 sender = 'system'
